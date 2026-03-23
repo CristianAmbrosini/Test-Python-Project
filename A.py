c551bfa383a4f2ce19f71c27324e7ba494797ced
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2026-03-23 15:21:50 +0100

    one more

--- A.py
+++ A.py
@@ -129,10 +129,6 @@ def add(a, b):
     # TODO: do something else!
     # TODO: do something!
     # TODO: do something else!
-    # TODO: do something!
-    # TODO: do something else!
-    # TODO: do something!
-    # TODO: do something else!
     return a + b
 
 
