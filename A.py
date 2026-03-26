d7c5b63e5553cc07650e5bc5a53e28883eb9fc18
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2026-03-26 14:39:56 +0100

    Revert "Test"
    This reverts commit 92e95dbdc555e448d6a60b5b2a37ab84066fc69a.

--- A.py
+++ A.py
@@ -131,6 +131,8 @@ def add(a, b):
     # TODO: do something else!
     # TODO: do something!
     # TODO: do something else!
+    # TODO: do something!
+    # TODO: do something else!
     return a + b
 
 
